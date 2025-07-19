fdf86ba59e31cf15f737d0986510daa4a2948426
ahmadtara <ahmadtara.pratama@gmail.com>
2025-07-20 01:16:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,9 @@ st.title("📍 KMZ ➜ HPDB (Auto-Fill)")
 kmz_file = st.file_uploader("Upload file .KMZ", type=["kmz"])
 template_file = st.file_uploader("Upload TEMPLATE HPDB (.xlsx)", type=["xlsx"])
 
+geolocator = Nominatim(user_agent="kmz-to-hpdb")
+geocode = RateLimiter(geolocator.reverse, min_delay_seconds=1)
+
 def extract_placemarks(kmz_bytes):
     def recurse_folder(folder, ns, path=""):
         placemarks = []
@@ -81,6 +84,30 @@ def find_matching_pole(fat, all_poles, tol=0.0001):
             return pole["name"]
     return "POLE_NOT_FOUND"
 
+def reverse_geocode(lat, lon):
+    try:
+        location = geocode((lat, lon), exactly_one=True, language='id')
+        if location and location.raw and 'address' in location.raw:
+            address = location.raw['address']
+            street = address.get("road", "")
+            district = address.get("suburb") or address.get("village") or address.get("residential") or ""
+            subdistrict = address.get("city_district") or address.get("district") or ""
+            postalcode = address.get("postcode", "")
+            return {
+                "street": street,
+                "district": district.upper(),
+                "subdistrict": subdistrict.upper(),
+                "postalcode": postalcode
+            }
+    except Exception as e:
+        print("Reverse geocode error:", e)
+    return {
+        "street": "",
+        "district": "",
+        "subdistrict": "",
+        "postalcode": ""
+    }
+
 if kmz_file and template_file:
     kmz_name = kmz_file.name.replace(".kmz", "")
     placemarks = extract_placemarks(kmz_file.read())
@@ -89,24 +116,10 @@ if kmz_file and template_file:
     fat_list = placemarks["FAT"]
     hp_list = placemarks["HP COVER"]
     fdtcode = placemarks["FDT"][0]["name"] if placemarks["FDT"] else "FDT_UNKNOWN"
-
     all_poles = placemarks["NEW POLE 7-3"] + placemarks["EXISTING POLE EMR 7-3"] + placemarks["EXISTING POLE EMR 7-4"]
 
-    # Inisialisasi geolocator
-    geolocator = Nominatim(user_agent="hpdb_app")
-    geocode = RateLimiter(geolocator.reverse, min_delay_seconds=1)
-
-    # Ambil informasi kelurahan/kecamatan/kode pos dari salah satu titik HP COVER
-    sample_hp = hp_list[0] if hp_list else None
-    if sample_hp:
-        location = geocode((sample_hp["lat"], sample_hp["lon"]), language='id')
-        address = location.raw.get("address", {}) if location else {}
-
-        postalcode = address.get("postcode", "")
-        district = (address.get("suburb") or address.get("village") or address.get("hamlet") or "").upper()
-        subdistrict = (address.get("city_district") or address.get("district") or "").upper()
-    else:
-        postalcode = district = subdistrict = ""
+    # Ambil info lokasi dari titik HP COVER pertama
+    lokasi_umum = reverse_geocode(hp_list[0]["lat"], hp_list[0]["lon"])
 
     row = 0
     for hp in hp_list:
@@ -119,21 +132,6 @@ if kmz_file and template_file:
         df_template.at[row, "Latitude_homepass"] = hp["lat"]
         df_template.at[row, "Longitude_homepass"] = hp["lon"]
 
-        # Ambil nama jalan dari titik koordinat masing-masing
-        location_hp = geocode((hp["lat"], hp["lon"]), language='id')
-        address_hp = location_hp.raw.get("address", {}) if location_hp else {}
-        street_name = (
-            address_hp.get("road") or
-            address_hp.get("residential") or
-            address_hp.get("neighbourhood") or
-            "JALAN_TIDAK_DIKETAHUI"
-        )
-
-        df_template.at[row, "postalcode"] = postalcode
-        df_template.at[row, "district"] = district
-        df_template.at[row, "subdistrict"] = subdistrict
-        df_template.at[row, "street"] = street_name
-
         matched_fat = find_fat_by_fatcode(fatcode, fat_list)
         if matched_fat:
             df_template.at[row, "FAT ID"] = matched_fat["name"]
@@ -144,9 +142,17 @@ if kmz_file and template_file:
             df_template.at[row, "FAT ID"] = "FAT_NOT_FOUND"
             df_template.at[row, "Pole ID"] = "POLE_NOT_FOUND"
 
+        # Isian tambahan
         df_template.at[row, "fdtcode"] = fdtcode
         df_template.at[row, "Clustername"] = kmz_name
         df_template.at[row, "Commercial_name"] = kmz_name
+        df_template.at[row, "postalcode"] = lokasi_umum["postalcode"]
+        df_template.at[row, "district"] = lokasi_umum["district"]
+        df_template.at[row, "subdistrict"] = lokasi_umum["subdistrict"]
+
+        # Ambil nama jalan berdasarkan titik HP
+        loc_detail = reverse_geocode(hp["lat"], hp["lon"])
+        df_template.at[row, "street"] = loc_detail["street"]
 
         row += 1
 
@@ -155,4 +161,6 @@ if kmz_file and template_file:
 
     output = BytesIO()
     df_template.to_excel(output, index=False)
-    st.download_button("📥 Download File Hasil", output.getvalue(), file_name="HASIL_HPDB_LENGKAP.xlsx")
+    output.seek(0)
+
+    st.download_button("📥 Download File Hasil", data=output, file_name="HASIL_HPDB_LENGKAP.xlsx")
