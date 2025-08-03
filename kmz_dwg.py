b3b6289d2fb8aee5020f327e487aa0ca26fb528c
ahmadtara <ahmadtara.pratama@gmail.com>
2025-08-03 11:01:34 +0700

    Update kmz_dwg.py

--- kmz_dwg.py
+++ kmz_dwg.py
@@ -262,6 +262,7 @@ def run_kmz_to_dwg():
 1️⃣ <span style='color:#FF6B6B;'>PASTIKAN KMZ SESUAI TEMPLATE</span>.<br>
 2️⃣ FOLDER KOTAK HARUS DIBUAT MANUAL DULU DARI DALAM KMZ <code>Agar kotak rumah otoatis didalam kode</code><br>
 """, unsafe_allow_html=True)
+               
     uploaded_kmz = st.file_uploader("📂 Upload File KMZ", type=["kmz"])
     uploaded_template = st.file_uploader("📀 Upload Template DXF", type=["dxf"])
 
@@ -288,4 +289,5 @@ def run_kmz_to_dwg():
                         st.download_button("⬇️ Download DXF", f, file_name="output_from_kmz.dxf")
             except Exception as e:
                 st.error(f"❌ Gagal memproses: {e}")
+
 
