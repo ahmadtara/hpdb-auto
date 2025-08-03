49468fbe953afd8a5e285427e05a21ac54c8b062
ahmadtara <ahmadtara.pratama@gmail.com>
2025-08-03 10:59:44 +0700

    Update kmz_dwg.py

--- kmz_dwg.py
+++ kmz_dwg.py
@@ -255,8 +255,13 @@ def draw_to_template(classified, template_path):
     return doc
 
 def run_kmz_to_dwg():
-    st.title("🏗️ KMZ → DXF (Masuk ke Template)")
-
+    st.title("🏗️ KMZ → AUTOCAD ")
+    st.markdown("""
+<h2>👋 Hai, <span style='color:#0A84FF'>obi</span></h2>
+✅ <span style='font-weight:bold;'>CATATAN PENTING :</span><br><br>
+1️⃣ <span style='color:#FF6B6B;'>PASTIKAN KMZ SESUAI TEMPLATE</span>.<br>
+2️⃣ FOLDER KOTAK HARUS DIBUAT MANUAL DULU DARI DALAM KMZ <code>Agar kotak rumah otoatis didalam kode</code><br>
+""", unsafe_allow_html=True)
     uploaded_kmz = st.file_uploader("📂 Upload File KMZ", type=["kmz"])
     uploaded_template = st.file_uploader("📀 Upload Template DXF", type=["dxf"])
 
@@ -283,3 +288,4 @@ def run_kmz_to_dwg():
                         st.download_button("⬇️ Download DXF", f, file_name="output_from_kmz.dxf")
             except Exception as e:
                 st.error(f"❌ Gagal memproses: {e}")
+
