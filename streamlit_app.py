33165d3518fd6d454072e9e858b3049e6d28f8d2
ahmadtara <ahmadtara.pratama@gmail.com>
2025-09-14 00:15:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="MyRepublic Toolkit", layout="wide")
 # -------------- ✅ USER LOGIN ---------------- #
 valid_users = {
     "zikni": "zikni",
-    "obi": "obi",
+    "kampang": "kampang",
     "toko": "toko"
 }
 blocked_users = set()
@@ -104,3 +104,4 @@ else:
         st.session_state["logged_in"] = False
         st.session_state["user"] = None
         st.rerun()
+
