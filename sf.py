4dae7e3a75a005f62baff44e48ebe268b46123ca
ahmadtara <ahmadtara.pratama@gmail.com>
2025-09-14 16:13:58 +0700

    Update sf.py

--- sf.py
+++ sf.py
@@ -208,7 +208,7 @@ def draw_to_template(classified, template_path):
 
             # kasih teks label
             msp.add_text(obj["name"], dxfattribs={
-                "height": 4.5,
+                "height": 5.0,
                 "layer": target_layer,
                 "insert": (x + 2, y)
             })
